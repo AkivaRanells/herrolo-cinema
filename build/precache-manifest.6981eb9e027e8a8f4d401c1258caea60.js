a340d37aeb7bc8f2b641aca5aa1d1721930b7077
Akiva Ranells <akranells@gmail.com>
2018-11-29 12:08:52 +0200

    still not working

---
+++ build/precache-manifest.6981eb9e027e8a8f4d401c1258caea60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33c9cf8695297f1f6e70",
+    "url": "/herrolo-cinema/static/js/runtime~main.33c9cf86.js"
+  },
+  {
+    "revision": "5089ee9c9509dda74f23",
+    "url": "/herrolo-cinema/static/js/main.5089ee9c.chunk.js"
+  },
+  {
+    "revision": "a70a6857fcb43582126b",
+    "url": "/herrolo-cinema/static/js/1.a70a6857.chunk.js"
+  },
+  {
+    "revision": "5089ee9c9509dda74f23",
+    "url": "/herrolo-cinema/static/css/main.d37c9b68.chunk.css"
+  },
+  {
+    "revision": "a70a6857fcb43582126b",
+    "url": "/herrolo-cinema/static/css/1.cd729ba3.chunk.css"
+  },
+  {
+    "revision": "6e44c018bc70e6e01e1cf61a08e214be",
+    "url": "/herrolo-cinema/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c286336277b6c33b26ff48fb636c725.js"
+  "/herrolo-cinema/precache-manifest.6981eb9e027e8a8f4d401c1258caea60.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/herrolo-cinema/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
