ac4d6441f37b51a7ef8337011cf8c4057fbff3fe
akivaranells <candidate6.travelfactory@hotmail.com>
2019-10-30 14:38:22 +0200

    added core-js attempting to solve ie issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
+import 'core-js/stable';
 import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
