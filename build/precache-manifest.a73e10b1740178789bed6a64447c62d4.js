cf9af96660f109d8dd8c8d4d9c7d9d4ed5eba5a2
akivaranells <candidate6.travelfactory@hotmail.com>
2019-10-30 14:49:34 +0200

    added regenerator-runtime to solve ie issue

---
+++ build/precache-manifest.a73e10b1740178789bed6a64447c62d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33c9cf8695297f1f6e70",
+    "url": "/herrolo-cinema/static/js/runtime~main.33c9cf86.js"
+  },
+  {
+    "revision": "86eec86d8e9885a58990",
+    "url": "/herrolo-cinema/static/js/main.86eec86d.chunk.js"
+  },
+  {
+    "revision": "f2c80c156d208c04ff2a",
+    "url": "/herrolo-cinema/static/js/1.f2c80c15.chunk.js"
+  },
+  {
+    "revision": "86eec86d8e9885a58990",
+    "url": "/herrolo-cinema/static/css/main.d16adbab.chunk.css"
+  },
+  {
+    "revision": "f2c80c156d208c04ff2a",
+    "url": "/herrolo-cinema/static/css/1.c5c1ceb2.chunk.css"
+  },
+  {
+    "revision": "bbeb390c44045b46231c0f54c3cbbed4",
+    "url": "/herrolo-cinema/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import 'babel-polyfill';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import 'core-js/stable';
-import React from 'react'
-import { render } from 'react-dom'
-import App from './containers/App'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import reducer from './reducers'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import React from 'react';
+import "regenerator-runtime/runtime";
+import { render } from 'react-dom';
+import App from './containers/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const middleware = [thunk]
