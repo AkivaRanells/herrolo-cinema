a93067a546ab8e19ea438a1e047b32885372e04b
akivaranells <candidate6.travelfactory@hotmail.com>
2019-10-30 14:56:51 +0200

    Updates

--- precache-manifest.02a6b3b7ff8aff3ab0c189755cadfab7.js
+++ precache-manifest.02a6b3b7ff8aff3ab0c189755cadfab7.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/herrolo-cinema/static/js/runtime~main.33c9cf86.js"
   },
   {
-    "revision": "0e49c2a840493010a10d",
-    "url": "/herrolo-cinema/static/js/main.0e49c2a8.chunk.js"
+    "revision": "06a21dcdc5986fe87d72",
+    "url": "/herrolo-cinema/static/js/main.06a21dcd.chunk.js"
   },
   {
-    "revision": "4dd1e85d6bbf4fe90e09",
-    "url": "/herrolo-cinema/static/js/1.4dd1e85d.chunk.js"
+    "revision": "c30d0466fd92ae5feae6",
+    "url": "/herrolo-cinema/static/js/1.c30d0466.chunk.js"
   },
   {
-    "revision": "0e49c2a840493010a10d",
+    "revision": "06a21dcdc5986fe87d72",
     "url": "/herrolo-cinema/static/css/main.66367b61.chunk.css"
   },
   {
-    "revision": "4dd1e85d6bbf4fe90e09",
+    "revision": "c30d0466fd92ae5feae6",
     "url": "/herrolo-cinema/static/css/1.9205eef5.chunk.css"
   },
   {
-    "revision": "e2c542358ae9e1beca196a1abcbeb045",
+    "revision": "f2bc322fcd3ddbfc112a8f3188282479",
     "url": "/herrolo-cinema/index.html"
   }
 ];
