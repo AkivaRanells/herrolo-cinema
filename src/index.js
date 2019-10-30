17e262e38d7d5c1d7fed29f2e060fa0b6ad94a8c
akivaranells <candidate6.travelfactory@hotmail.com>
2019-10-30 14:34:19 +0200

    updated babel import statement syntax

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
-import babelPolyfill from 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
